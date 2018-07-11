581a0c762d72a87e4adb799891776442a7804e70
Alberto Grau <agrau@caudalie-usa.com>
2018-07-11 17:07:11 -0400

    Console logs

--- client/worker.js
+++ client/worker.js
@@ -1,5 +1,6 @@
 self.addEventListener('push', e => {
     const data = e.data.json();
+    console.log('Received!');
     self.registration.showNotification(data.title, {
         body: 'Flash sale! Hurry only 30mins left!',
         icon: 'https://upload.wikimedia.org/wikipedia/commons/0/0a/Caudalie_Logo.jpg'
