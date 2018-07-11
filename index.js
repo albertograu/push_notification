295a95176a5b5de7385f02d01c1946372fff2617
Alberto Grau <agrau@caudalie-usa.com>
2018-07-11 12:37:25 -0400

    Server setup

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const webpush = require('web-push');
+const bodyparser = require('body-parser');
+const path = require('path');
+
+// Initialize Express Server
+const app = express();
+
+// Set static path
+app.use(express.static(path.join(__dirname, 'client')));
+
+// VAPID Keys (Id's whose sending push notifications)
+require('dotenv').config();
+const publicVapidKey = process.env.PUBLIC_VAPID_KEY;
+const privateVapidKey = process.env.PRIVATE_VAPID_KEY;
+
+// body-parser Middleware
+app.use(bodyparser.json());
+
+webpush.setVapidDetails('mailto:test@test.com', publicVapidKey, privateVapidKey);
+
+// Subscribe Route
+app.post('/subscribe', (req, res) => {
+    // Get pushSubscription object 
+    const subscription = req.body;
+
+    // Send 201 - resource created
+    res.status(201).json({});
+
+    // Create payload
+    const payload = JSON.stringify({
+        title: 'Push Test'
+    });
+
+    // Pass object to sendNotification
+    webpush.sendNotification(subscription, payload)
+        .catch(err => console.error(err));
+});
+
+
+const port = 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
